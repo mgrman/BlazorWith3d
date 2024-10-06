f2271a577bb83b92e309399bf6488acdb48e862c
Martin Grman <martingrman@outlook.com>
2024-10-06 14:11:55 +0200

    wip

--- samples/BlazorWith3d.ExampleApp.UnityProject/Assets/ExampleApp/ExampleAppInitializer.cs
+++ samples/BlazorWith3d.ExampleApp.UnityProject/Assets/ExampleApp/ExampleAppInitializer.cs
@@ -9,42 +9,40 @@ using UnityEngine.InputSystem;
 
 namespace ExampleApp
 {
-    public class ExampleAppInitializer :MonoBehaviour
+    public class ExampleAppInitializer : MonoBehaviour
     {
-        private readonly Dictionary<int, BlockController> _templates=new ();
-        private readonly Dictionary<int, BlockController > _blocks=new ();
+        private readonly Dictionary<int, BlockController> _templates = new();
+        private readonly Dictionary<int, BlockController> _blocks = new();
         private GameObject _templateRoot;
-        
-        [SerializeField]
-        private BlockController _templatePrefab;
-        
-        [SerializeField]
-        private GameObject _backgroundPlane;
 
+        [SerializeField] 
+        private BlockController _templatePrefab;
+
+        [SerializeField] 
+        private GameObject _backgroundPlane;
+        
         private TypedBlazorApi _typedApi;
         private IBlazorApi _blazorApi;
 
         public void Start()
         {
-            _templateRoot=new GameObject($"BlockTemplateRoot");
+            _templateRoot = new GameObject($"BlockTemplateRoot");
             _templateRoot.SetActive(false);
             _templateRoot.transform.parent = transform;
 
-            // TODO create a simulator!!!
-            
-            #if UNITY_EDITOR
-            
+#if UNITY_EDITOR
+
             var simulatorApi = new SimulatorApi();
             var simulatorTypedApi = new UnityTypedUnityApi(simulatorApi);
             var simulator = gameObject.AddComponent<BlazorSimulator>();
             gameObject.AddComponent<DragChangingSimulatorHandler>();
             simulator.Initialize(simulatorTypedApi, typeof(AppInitialized).Assembly);
             _blazorApi = simulatorApi;
-            #else
+#else
             _blazorApi = new UnityBlazorApi();
-            #endif
+#endif
             _typedApi = new TypedBlazorApi(_blazorApi);
-            
+
             _typedApi.AddMessageProcessCallback<AddBlockTemplateMessage>(OnAddBlockTemplateMessage);
             _typedApi.AddMessageProcessCallback<RemoveBlockTemplateMessage>(OnRemoveBlockTemplateMessage);
             _typedApi.AddMessageProcessCallback<AddBlockInstanceMessage>(OnAddBlockInstanceMessage);
@@ -59,24 +57,25 @@ namespace ExampleApp
                 TemplateId = 0,
                 SizeX = 1, SizeY = 1, SizeZ = 1, VisualsUri = null
             });
-            simulatorTypedApi.SendMessage(new AddBlockInstanceMessage(){ BlockId = 0, TemplateId = 0, PositionX = 0, PositionY = 0, RotationZ = 0});
+            simulatorTypedApi.SendMessage(new AddBlockInstanceMessage()
+                { BlockId = 0, TemplateId = 0, PositionX = 0, PositionY = 0, RotationZ = 0 });
 #endif
         }
 
         private void Update()
         {
-          //  Debug.Log(EventSystem.current.currentSelectedGameObject);
+            //  Debug.Log(EventSystem.current.currentSelectedGameObject);
         }
 
         private void OnAddBlockTemplateMessage(AddBlockTemplateMessage msg)
         {
             Debug.Log($"Adding block template: {JsonUtility.ToJson(msg)}");
-            
-            
+
+
             var meshGo = GameObject.Instantiate(_templatePrefab, _templateRoot.transform);
-            
-            meshGo.Initialize(msg,gameObject, _typedApi);
-            
+
+            meshGo.Initialize(msg, gameObject, _typedApi);
+
             _templates.Add(msg.TemplateId, meshGo);
 
             Debug.Log($"Added block template: {JsonUtility.ToJson(msg)}");
@@ -92,21 +91,21 @@ namespace ExampleApp
         private void OnAddBlockInstanceMessage(AddBlockInstanceMessage msg)
         {
             Debug.Log($"Adding block : {JsonUtility.ToJson(msg)}");
-            var template=_templates[msg.TemplateId];
+            var template = _templates[msg.TemplateId];
 
 
-            var instance=template.CreateInstance(msg);
-             _blocks.Add(msg.BlockId,instance );
-             Debug.Log($"Added block : {JsonUtility.ToJson(msg)}");
+            var instance = template.CreateInstance(msg);
+            _blocks.Add(msg.BlockId, instance);
+            Debug.Log($"Added block : {JsonUtility.ToJson(msg)}");
         }
 
         private void OnRemoveBlockMessage(RemoveBlockMessage msg)
         {
             Debug.Log($"Removing block template: {JsonUtility.ToJson(msg)}");
-            var blockGo=_blocks[msg.BlockId];
+            var blockGo = _blocks[msg.BlockId];
             GameObject.Destroy(blockGo);
             _blocks.Remove(msg.BlockId);
-            
+
             Debug.Log($"Removed block template: {JsonUtility.ToJson(msg)}");
         }
     }
