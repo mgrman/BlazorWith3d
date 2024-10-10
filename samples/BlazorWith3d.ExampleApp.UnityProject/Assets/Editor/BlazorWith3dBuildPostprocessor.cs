716ded78fc0f12bc18d1c74585fabd42b36342a4
Martin Grman <martingrman@outlook.com>
2024-10-11 00:25:12 +0200

    fix

--- samples/BlazorWith3d.ExampleApp.UnityProject/Assets/Editor/BlazorWith3dBuildPostprocessor.cs
+++ samples/BlazorWith3d.ExampleApp.UnityProject/Assets/Editor/BlazorWith3dBuildPostprocessor.cs
@@ -1,23 +1,23 @@
-﻿// #if UNITY_EDITOR
-// using System.IO;
-// using UnityEditor;
-// using UnityEditor.Callbacks;
-// using UnityEngine;
-//
-// public class BlazorWith3dBuildPostprocessor {
-//     
-//     [PostProcessBuild(1)]
-//     public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
-//     {
-//         var buildFilesFolder =Path.GetFullPath( Path.Combine(pathToBuiltProject, "Build"));
-//         var backendWwwrootFolder = Path.GetFullPath(Path.Combine("..", "BlazorWith3d.ExampleApp.Client","wwwroot"));
-//
-//         foreach (var file in Directory.GetFiles(buildFilesFolder))
-//         {
-//             File.Copy(file,Path.Combine(backendWwwrootFolder,Path.GetFileName(file)),true);
-//         }
-//         Debug.Log("BlazorWith3d.ExampleApp.Client updated with fresh build!");
-//     }
-// }
-//
-// #endif
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class BlazorWith3dBuildPostprocessor {
+    
+    [PostProcessBuild(1)]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        var buildFilesFolder =Path.GetFullPath( Path.Combine(pathToBuiltProject, "Build"));
+        var backendWwwrootFolder = Path.GetFullPath(Path.Combine("..", "BlazorWith3d.ExampleApp.Client","wwwroot"));
+
+        foreach (var file in Directory.GetFiles(buildFilesFolder))
+        {
+            File.Copy(file,Path.Combine(backendWwwrootFolder,Path.GetFileName(file)),true);
+        }
+        Debug.Log("BlazorWith3d.ExampleApp.Client updated with fresh build!");
+    }
+}
+
+#endif
