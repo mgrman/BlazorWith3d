354ccdd4d5a7f30890b2072a7290814afd5f5697
Martin Grman <martingrman@outlook.com>
2024-10-11 00:43:10 +0200

    wip

--- samples/BlazorWith3d.ExampleApp.Client.Unity.Shared/Messages.cs
+++ samples/BlazorWith3d.ExampleApp.Client.Unity.Shared/Messages.cs
@@ -3,14 +3,14 @@ using BlazorWith3d.Unity.Shared;
 
 namespace BlazorWith3d.ExampleApp.Client.Unity.Shared
 {
-// ## Methods.v2
-// - void AddBlockTemplate(int templateId, float sizeX, float sizeY,float sizeZ, string visualsUri)
-// - void AddBlockInstance(int blockId, float positionX, float positionY, float rotationZ, int templateId)
-// - void RemoveBlock(int blockId)
-// - void RemoveBlockTemplate(int templateId)
-// - void SubscribeToBlockPoseChanging(Func<int blockId, float positionX, float positionY, float rotationZ, (float positionX, float positionY, float rotationZ)> onPoseValidation)
-// - void SubscribeToBlockPoseChanged(Action<int blockId, float positionX, float positionY, float rotationZ> onPoseChanged)
-// - void StartDraggingBlock(int blockId, int templateId)
+    // ## Methods.v2
+    // - void AddBlockTemplate(int templateId, float sizeX, float sizeY,float sizeZ, string visualsUri)
+    // - void AddBlockInstance(int blockId, float positionX, float positionY, float rotationZ, int templateId)
+    // - void RemoveBlock(int blockId)
+    // - void RemoveBlockTemplate(int templateId)
+    // - void SubscribeToBlockPoseChanging(Func<int blockId, float positionX, float positionY, float rotationZ, (float positionX, float positionY, float rotationZ)> onPoseValidation)
+    // - void SubscribeToBlockPoseChanged(Action<int blockId, float positionX, float positionY, float rotationZ> onPoseChanged)
+    // - void StartDraggingBlock(int blockId, int templateId)
 
     public sealed class AppInitialized : IMessageToBlazor<AppInitialized>
     {
@@ -24,7 +24,7 @@ namespace BlazorWith3d.ExampleApp.Client.Unity.Shared
         public string? VisualsUri;
     }
 
-    
+
     public sealed class AddBlockInstanceMessage : IMessageToUnity<AddBlockInstanceMessage>
     {
         public int BlockId;
@@ -34,26 +34,26 @@ namespace BlazorWith3d.ExampleApp.Client.Unity.Shared
         public int TemplateId;
     }
 
-    
+
     public sealed class RemoveBlockMessage : IMessageToUnity<RemoveBlockMessage>
     {
         public int BlockId;
     }
 
-    
+
     public sealed class RemoveBlockTemplateMessage : IMessageToUnity<RemoveBlockTemplateMessage>
     {
         public int TemplateId;
     }
 
-    
+
     public sealed class StartDraggingBlockMessage : IMessageToUnity<StartDraggingBlockMessage>
     {
         public int TemplateId;
         public int BlockId;
     }
 
-    
+
     public sealed class PoseChangeResponse
     {
         public bool IsValid;
@@ -62,7 +62,7 @@ namespace BlazorWith3d.ExampleApp.Client.Unity.Shared
         public float NewRotationZ;
     }
 
-    
+
     public sealed class BlockPoseChangingMessage : IMessageToBlazor<BlockPoseChangingMessage, PoseChangeResponse>
     {
         public int BlockId;
@@ -71,7 +71,7 @@ namespace BlazorWith3d.ExampleApp.Client.Unity.Shared
         public float RotationZ;
     }
 
-    
+
     public sealed class BlockPoseChangedMessage : IMessageToBlazor<BlockPoseChangedMessage>
     {
         public int BlockId;
